4a864de083d4a59a55fffad26ec59093612999a4
yjzhang <719036891@qq.com>
2023-12-26 14:32:35 +0800

    更换实验数据

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,17 @@ function App() {
     useEffect(() => {
         if (!cesiumContainer) return
 
-        const viewer = new Cesium.Viewer(cesiumContainer.current);
+        const viewer = new Cesium.Viewer(cesiumContainer.current, {
+            animation: false, // 左下角 圆盘动画控件
+            timeline: false, //时间轴
+            fullscreenButton: false, //右下角 全屏控件
+            // scene3DOnly: true, // 每个几何实例仅以3D渲染以节省GPU内存
+        });
+        viewer._cesiumWidget._creditContainer.style.display = "none"; //隐藏版本信息
 
         let provider = new Cesium.WebMapTileServiceImageryProvider({        //创建一个图层（geoserver中的tif）
-            url: 'http://127.0.0.1:8081/geoserver/gwc/service/wmts/rest/cesium-demo:ASTGTM2_N05W058_dem/{style}/{TileMatrixSet}/{TileMatrix}/{TileRow}/{TileCol}?format=image/png',
-            layer: 'cesium-demo:ASTGTM2_N05W058_dem',
+            url: 'http://localhost:8081/geoserver/gwc/service/wmts/rest/cesium-demo:LC08_L2SP_121039_20220115_20220123_02_T1_SR_B1/{style}/{TileMatrixSet}/{TileMatrix}/{TileRow}/{TileCol}?format=image/png',
+            layer: 'cesium-demo:LC08_L2SP_121039_20220115_20220123_02_T1_SR_B1',
             style: '',
             format: 'image/png',
             tilingScheme: new Cesium.GeographicTilingScheme(),
@@ -24,7 +30,7 @@ function App() {
         viewer.imageryLayers.addImageryProvider(provider);
 
         viewer.camera.setView({
-            destination: Cesium.Cartesian3.fromDegrees(-57.48,5.83, 300000.0),   // 经纬度和高度
+            destination: Cesium.Cartesian3.fromDegrees(-57.48, 5.83, 300000.0),   // 经纬度和高度
         });
     }, [cesiumContainer.current])
 
